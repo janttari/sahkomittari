334ae70c0d5ed60a7c63b28205fdf2daaaa5266d
Jani Janttari <janttari@yandex.ru>
2020-07-03 15:32:40 +0300

    gup

--- raspberry-client/src/opt/sahkomittari/raspisahkomittari.py
+++ raspberry-client/src/opt/sahkomittari/raspisahkomittari.py
@@ -1,153 +1,131 @@
 #!/usr/bin/env python3
-#sudo pip3 install websocket-client
 import time, os, sys, socket, threading, websocket, configparser
-from multiprocessing import Process, Pipe
 import RPi.GPIO as GPIO
 #----------------------------------------------------------------
 skriptinHakemisto=os.path.dirname(os.path.realpath(__file__)) #Tämän skriptin fyysinen sijainti configia varten
 config = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
 config.read(skriptinHakemisto+'/sahkomittari.ini')
-PALVELIN=config['yleiset']['palvelin']
-PULSSIPINNI=int(config['yleiset']['pulssipinni'])
-imp=int(config['yleiset']['imp'])
-maxLahetysTiheys=float(config['yleiset']['maxtiheys'])
-maxAliveTiheys=float(config['yleiset']['alive'])
-pulssiPysyva=config['yleiset']['pulssipysyva']
-tallennaPulssiSek=float(config['yleiset']['tallennapulssisek'])
-yks=1000/imp #yksi pulssi on näin monta wattia
-pulssiLaskuri=0 #lasketaan tähän muuttujaan pulssit
-edLahetysAika=0 #Edellisen kerran lähetetty palvelimelle dataa aikaleima
-edPulssi=0 #Edellisen kerran saadun pulssin aikaleima
-tallennettuPulssi=0 #Viimeisin tiedostoon tallennettu pulssilukema, ettei turhaan kirjoitella jos se ei ole muuttunut
-viimLahetysaika=0 #websocketille on viimeksi lähetetty-aikaleima
-viimLahetettyPulssi=0 #viimeksi websocketille lähetetty pulssimäärä
-kwh="" #kokonaiskulutus kwh
-reaaliaikainen="" #reaaliaikainen kulutus tällä hetkellä
-aja=True #liipaise tämä Falseksi niin lopetetaan
-
-GPIO.setmode(GPIO.BCM) #https://www.raspberrypi-spy.co.uk/2012/06/simple-guide-to-the-rpi-gpio-header-and-pins/
-GPIO.setup(PULSSIPINNI, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #käytetään sisäistä alasvetoa
 
 def lokita(rivi):
     kello=time.strftime("%y%m%d-%H%M%S")
     print(kello, rivi)
     sys.stdout.flush()
 
-def luePinni(child_conn): #Lukee GPIO-pinniltä pulsseja. Tämä käynnistetään itsenäisenä prosessina
-    global PULSSIPINNI
-    while True:
-        GPIO.wait_for_edge(PULSSIPINNI, GPIO.RISING) #odotetaan nouseva reuna
-        child_conn.send(1) #Lähetetään että on saatu pulssi
-        GPIO.wait_for_edge(PULSSIPINNI, GPIO.FALLING) #odotetaan laskeva reuna
+class Mittaaja(): # TÄMÄ LUOKKA HOITAA VARSINAISEN PINNIN LUKEMISEN JA KULUTUKSEEN LIITTYVÄN LASKENNAN --------------------------
+    def __init__(self, callback):
+        '''self, pulssipinni, viestikanava, pulssiValue, maxLahetysTiheys, maxAliveTiheys, imp_per_kwh'''
+        self.callback=callback
+        self.pulssipinni=int(config['yleiset']['pulssipinni'])
+        self.pulssilaskuri = -1 #lasketaan tähän pulssit
+        self.maxLahetysTiheys=float(config['yleiset']['maxtiheys'])
+        self.maxAliveTiheys=float(config['yleiset']['alive'])
+        self.imp=int(config['yleiset']['imp'])
+        self.viimLahetys = 0 #unix aika, milloin on viimeksi lähetetty lukemat
+        self.viimPulssimaara = 0 #viimeisen lähetyksen pulssimäärä
+        self.viimPulssiAika = 0  #viimeisimmän pulssin aikaleima
+        self.aikaaEdPulssista = 1
+        self.pinninLukija = threading.Thread(target=self.luePinni) #Lukee pinnin tilan prosessi
+        self.pinninLukija.start()
+        self.valvoja=threading.Thread(target=self.valvoPulsseja)
+        self.valvoja.start()
 
-def vastaanotaImpulssi(parent_conn): #Tämä säie vastaanottaa aina luvun 1 kun luePinni on lukenut pulssin
-    global pulssiLaskuri, aja, edPulssi
-    while aja:
-        data=parent_conn.recv()
-        #lokita("saatu pulssi")
-        if data != "stop": #Ohjelman alasajoon liityvä snoma
-            pulssiLaskuri+=1
-            edPulssi=time.time()
+    def luePinni(self): #Varsinainen pinniä lukeva osa
+        GPIO.setmode(GPIO.BCM) #https://www.raspberrypi-spy.co.uk/2012/06/simple-guide-to-the-rpi-gpio-header-and-pins/
+        GPIO.setup(self.pulssipinni, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #käytetään sisäistä alasvetoa
+        while True:
+            GPIO.wait_for_edge(self.pulssipinni, GPIO.RISING) #odotetaan nouseva reuna
+            self.aikaaEdPulssista=time.time()-self.viimPulssiAika
+            self.pulssilaskuri+=1
+            self.viimPulssiAika=time.time()
+            GPIO.wait_for_edge(self.pulssipinni, GPIO.FALLING) #odotetaan laskeva reuna
+    
+    def valvoPulsseja(self): #Tämä seuraa saapuvia pulsseja ja milloin pitää lähettää alive
+        time.sleep(3) #Odotetaan hetki kunnes tallennetut lukemat on saatu varmasti
+        while True:
+            if (time.time()-self.viimLahetys>self.maxLahetysTiheys and self.viimPulssimaara != self.pulssilaskuri) or (time.time()-self.viimLahetys>self.maxAliveTiheys):
+                self.viimLahetys=time.time()
+                if self.viimPulssimaara != self.pulssilaskuri: #jos pulsseja on tullut
+                    self.viimPulssimaara = self.pulssilaskuri
+                    tmpInfo="kulutus"
+                else:
+                    tmpInfo="alive"
+                    self.aikaaEdPulssista=time.time()-self.viimPulssiAika
+                tmpPulssit=int(self.pulssilaskuri)
+                tmpKwh="{:.5f}".format(tmpPulssit*1000/self.imp/1000) #kokonaiskulutus kwh
+                tmpReaaliaikainen="{:.5f}".format(((1000/self.imp*3600)/self.aikaaEdPulssista)/1000) #kulutusta on tällä hetkellä kW
+                self.callback((tmpPulssit, tmpKwh, tmpReaaliaikainen, tmpInfo)) #tuple (int pulssimäärä, str kokonaiskulutus, str reaaliaik, str info)            
+            time.sleep(0.05)
+            
+    def setPulssilukema(self, lukema): #Voidaan asettaa pulssien määrä
+        self.pulssilaskuri=lukema
+        lokita("asetetaan mittarilukema: "+str(self.pulssilaskuri))
+        
+    def getPulssilukema(self):
+        return self.pulssilaskuri
 
-def reconnect(): #Uudelleenyhdistää katkenneen yhteyden sulkemalla wsasiakas-threadin ja avaamalla sen uudelleen
-    global threadWsAsiakas
-    threadWsAsiakas.join()
-    time.sleep(1)
-    lokita("***reconnect")
-    threadWsAsiakas=threading.Thread(target=wsasiakas)
-    threadWsAsiakas.start()
+class WsAsiakas(): #-----------------------------------------------------------------------------------------------------
+    def __init__(self):
+        self.palvelin=config['yleiset']['palvelin']
+        self.t=threading.Thread(target=self.wsYhteys)
+        self.t.start()
 
-def on_open(ws): #Tämä suoritetaan kun ws-yhteyn on avattu
-    lokita("ws avattu")
+    def wsYhteys(self):
+        websocket.enableTrace(False)
+        self.ws = websocket.WebSocketApp(self.palvelin,
+                                  on_message = self.on_message,
+                                  on_error = self.on_error,
+                                  on_close = self.on_close,
+                                  on_open = self.on_open)
+        self.ws.run_forever()
+        
+    def on_message(self, ws, message):
+        pass
 
-def on_message(ws, message): #Tämä suoritetaan kun serveri lähettää meillepäin ws dataa
-    pass
+    def on_error(self, error):
+        lokita("WS error: "+str(error))
 
-def on_error(ws, error): #ws virhe
-    lokita("wserror "+str(error))
+    def on_close(self, ws):
+        lokita("WS close")
 
-def on_close(ws): #Tämä tapahtuu kun ws yhteys on katkennut
-    lokita("ws katkennut")
-    reconnect() #Yritetään avata yhteys uudelleen
+    def on_open(self, ws):
+        lokita("WS open")
 
-def lahetaLukema(): #Tämä säie lähettää websocketille tiedot
-    global aja, pulssiLaskuri, viimLahetettyPulssi, maxLahetysTiheys, maxAliveTiheys, viimLahetysaika, kwh, reaaliaikainen, edPulssi
-    time.sleep(2) #ettei eka viesti lähde ennen kuin socket auki
-    while aja:
-        if pulssiLaskuri!= viimLahetettyPulssi and  time.time()-viimLahetysaika > maxLahetysTiheys: #pulssien määrä on muuttunut
-            info = "-"
-            viimLahetettyPulssi=pulssiLaskuri
-            viimLahetysaika=time.time()
-        elif time.time()-viimLahetysaika > maxAliveTiheys: #pulssit ei muuttuneet, alive-viesti
-            info = "alive"
-            viimLahetysaika=time.time()
-        if info != "": #Jos on jotain lähetettävää...
-            ero=time.time()-edPulssi
-            kwh="{:.5f}".format(pulssiLaskuri*yks/1000) #Yhteensä kulutusta kertynyt kW
-            reaaliaikainen="{:.5f}".format(1000/yks/ero/1000) #kulutusta on tällä hetkellä kW
-            rivi='{"kwh": "'+kwh+'", "pulssit": "'+str(pulssiLaskuri)+'", "reaaliaikainen": "'+reaaliaikainen+'", "info": "'+info+'"}'
-            lahetaWs(rivi)
-            info=""
-        time.sleep(0.05)
-
-def lahetaWs(sanoma): #Lähetä sanoma serverille päin
-    if kwh != "": #jos ei ole ohjelman esimmäinen suoritukierros, jolla ei vilä lähetettävää ole
-        #print(sanoma)
+    def lahetaWs(self, sanoma):
         try:
-            ws.send(sanoma)
-        except: #Jos lähetys ei onnistu...
+            self.ws.send(sanoma)
+        except: #jos lähetys ei onnistu
             lokita("ws Virhe viestin lähetyksessä!")
-            reconnect() #Pyydetään avaamaan ws uudelleen
+            self.reconnect() #Pyydetään avaamaan ws uudelleen
 
-def wsasiakas(): #Varsinainen ws-client. Suorita tähä threadina!
-    global ws, PALVELIN
-    websocket.enableTrace(False)
-    ws = websocket.WebSocketApp(PALVELIN,
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close,
-                              on_open = on_open)
-    ws.run_forever()
+    def reconnect(self): #avaa ws uudelleen
+        self.t.join()
+        time.sleep(1)
+        lokita("***reconnect ws")
+        self.t=threading.Thread(target=self.wsYhteys)
+        self.t.start()
+#------------------------------------------------------------------------------------------------------------------------------------
+
+def vastaanotaImpulssi(data): #Tämä kutsutaan kun pulssien saatu
+    pulssimaara, kwh, reaaliaik, info = data
+    rivi='{"kwh": "'+kwh+'", "pulssit": "'+str(pulssimaara)+'", "reaaliaikainen": "'+reaaliaik+'", "info": "'+info+'"}'
+    wsAsiakas.lahetaWs(rivi)
 
 def tallennaPulssi(): # Tallentaa pulssilukeman pysyväksi
     lokita("tallenapulssi pysyvään tiedostoon")
-    tallennettuPulssi=pulssiLaskuri
-    with open(pulssiPysyva, "w") as fpulssiTallenna: #tallennetaan pulssien määrä pysyväksi
-        fpulssiTallenna.write(str(pulssiLaskuri))
-
-if __name__ == "__main__":
-    parent_conn, child_conn = Pipe() #Pinniä lukeva prosessi kommunikoi tän kautta
-    threadWsAsiakas=threading.Thread(target=wsasiakas) #WS thread
-    threadWsAsiakas.start()
-    kasit=threading.Thread(target=vastaanotaImpulssi, args=(child_conn,)) #Vastaanottaa pulssit thread
-    kasit.start()
-    lahettelija=threading.Thread(target=lahetaLukema) #Lähettää pulssit palvelimelle thread
-    lahettelija.start()
-    process = Process(target=luePinni, args=(parent_conn,)) #Lukee pinnin tilan prosessi
-    process.start()
-    kierros=0
-    time.sleep(2) #ettei lähetä ennen kuin soketti auki
-    if os.path.isfile(pulssiPysyva): #Jos on olemassa tallennettu pulssilukema
-        with open(pulssiPysyva, "r") as pulssiTiedosto: #Luetaan pulssilukema tiedostosta
-            pulssiLaskuri=int(pulssiTiedosto.read())
-    try:
-        while aja: #Suoritetaan tätä looppia ja tehdään täällä säännöllisesti tarvittavat toiminnot
-            if kierros % tallennaPulssiSek == 0 and tallennettuPulssi != pulssiLaskuri and kierros !=0:
-                tallennaPulssi()
-            time.sleep(1)
-            kierros+=1
-    except KeyboardInterrupt: #Testikäytössä ohjelma on hankala sammuttaa, joudutaan vähän kikkailemaan
-        lokita("LOPETETAAN")
-        tallennaPulssi()
-        aja=False
-        ws.close()
-        threadWsAsiakas.join()
-        parent_conn.send("stop")
-        kasit.join()
-        lahettelija.join()
-        process.terminate()
-        GPIO.cleanup()
-        lokita("LOPETETTU")
-
-
-
+    with open(config['yleiset']['pulssipysyva'], "w") as fpulssiTallenna: #tallennetaan pulssien määrä pysyväksi
+        fpulssiTallenna.write(str(mittari.getPulssilukema()))
+                
+if __name__ == "__main__": #----------------------------------------------------------------------------------------------------------
+    wsAsiakas=WsAsiakas()
+    viimtallennettuPulssiAika=time.time() #aika jolloin pulssi on viimeksi tallennettu tiedostoon
+    mittari=Mittaaja(vastaanotaImpulssi)
+    time.sleep(0.5)
+    if os.path.isfile(config['yleiset']['pulssipysyva']): #Jos on olemassa tallennettu pulssilukema
+        lokita("lue kulutus tiedostosta")
+        with open(config['yleiset']['pulssipysyva'], "r") as pulssiTiedosto: #Luetaan pulssilukema tiedostosta
+            mittari.setPulssilukema(int(pulssiTiedosto.read()))
+    while True:
+        if time.time() - viimtallennettuPulssiAika >= float(config['yleiset']['tallennapulssisek']):
+            tallennaPulssi()
+            viimtallennettuPulssiAika=time.time()
+        time.sleep(3)
