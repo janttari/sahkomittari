198f9a72b62cf6d0161f4bf03de8a2dd93978182
Jani Janttari <janttari@yandex.ru>
2020-08-15 11:05:17 +0300

    gup

--- linux-server/src/www/kuukausiraportit/raportti.php
+++ linux-server/src/www/kuukausiraportit/raportti.php
@@ -1,9 +1,7 @@
 <!doctype html>
-
 <html lang="fi">
 <head>
   <meta charset="utf-8">
-
   <title>Raportti</title>
 </head>
 
@@ -13,11 +11,6 @@
         $vuosi=$_GET["vuosi"];
         $kuukausi=$_GET["kuukausi"];
         date_default_timezone_set('Europe/Helsinki');
-        $skkalku = $vuosi."-".$kuukausi."-"."01";
-        $ukkalku = mktime(0, 0, 0, $kuukausi, 1, $vuosi ); //kuukauden ensimmäinen ajankohta unix-aikana
-        $skkviimpv= date("t", strtotime($skkalku)); //kyseisen kuukauden viimeisen päivän numero
-        $ukkloppu = mktime(23, 59, 59, $kuukausi, $skkviimpv, $vuosi); //kuukauden viimeinen ajankohta unix-aikana
-        //echo $ukkalku . "-". $ukkloppu;
         $db_asiakkaat = new SQLite3('/opt/sahkomittari-server/data/asiakkaat.db');
         $db_kulutus = new SQLite3('/opt/sahkomittari-server/data/kulutus.db');
         $sql = "SELECT * from asiakkaat";
@@ -26,7 +19,7 @@
         echo "ip;numero;nimi;kulutus\n";
         while($row = $ret_asiakkaat->fetchArray(SQLITE3_ASSOC) ) {
             //Kysellään kuukauden ensimmäinen kulutuslukema, koska siinä on edellisen kuukauden loppulukema
-            $sql = "SELECT * from kulutus WHERE ip='".$row['ip']."' AND aikaleima BETWEEN ".$ukkalku." AND ".$ukkloppu." ORDER BY aikaleima LIMIT 1";
+            $sql = "SELECT * from kulutus WHERE ip='".$row['ip']."' AND strftime('%Y-%m', aikaleima) = '".$vuosi."-".$kuukausi."'  ORDER BY aikaleima LIMIT 1";
             $ret_kulutus = $db_kulutus->query($sql);
             $row_kulutus = $ret_kulutus->fetchArray(SQLITE3_ASSOC);
             $kwh=$row_kulutus['kwh'];
@@ -38,7 +31,6 @@
         echo "</pre>";
         $db_asiakkaat->close();
         $db_kulutus->close();
-
     ?>
 </body>
 </html>
---
+++ linux-server/temp-muunna-tietokanta.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import time, datetime, pytz, os, sqlite3
+#muuttaa tietokannan unix-time --> YYYY-MM-DD HH:MM:SS (paikallisaika)
+tietokanta="/opt/sahkomittari-server/data/kulutus.db"
+tmptietokanta="tmp.db"
+
+if os.path.isfile(tmptietokanta):
+    print("rm temp")
+    os.remove(tmptietokanta)
+
+conn = sqlite3.connect(tietokanta)
+connu = sqlite3.connect(tmptietokanta)
+c = conn.cursor()
+cu = connu.cursor()
+cu.execute('CREATE TABLE IF NOT EXISTS kulutus (aikaleima INTEGER, ip TEXT , kwh REAL, pulssit INTEGER, tuntikohtainen REAL, lampo REAL, kosteus REAL, ulkolampo REAL, ulkokosteus REAL)')
+kys=c.execute('SELECT * FROM kulutus')
+for i in kys:
+    aikaleima, ip, kwh, pulssit, tuntikohtainen, lampo, kosteus, ulkolampo, ulkokosteus = i
+    unix_timestamp = int(aikaleima)
+    paikaika = time.strftime("%Y-%m-%d %H:%M:%S",(time.localtime(unix_timestamp)))
+    #    print(paikaika,type(paikaika))
+    cu.execute('INSERT into kulutus(aikaleima, ip, kwh, pulssit, tuntikohtainen, lampo, kosteus, ulkolampo, ulkokosteus) VALUES("'+str(paikaika)+'", "'+str(ip)+'", "'+str(kwh)+'", "'+str(pulssit)+'", "'+str(tuntikohtainen)+'", "'+str(lampo)+'", "'+str(kosteus)+'", "'+str(ulkolampo)+'", "'+str(ulkokosteus)+'")')
+connu.commit()
+conn.close()
+connu.close()
+os.rename(tmptietokanta, tietokanta)
+
